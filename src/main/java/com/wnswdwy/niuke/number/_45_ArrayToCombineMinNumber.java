ff161aff7b5e0860f5a750417cd1202cb3ea3f87
nswdwy_yu <nswdwy_yu@163.com>
2021-02-14 18:43:04 +0800

    day13

--- src/main/java/com/wnswdwy/niuke/number/_45_ArrayToCombineMinNumber.java
+++ src/main/java/com/wnswdwy/niuke/number/_45_ArrayToCombineMinNumber.java
@@ -1,5 +1,7 @@
 package com.wnswdwy.niuke.number;
 
+import java.util.Arrays;
+
 /**
  * @author yycstart
  * @create 2021-02-14 17:17
@@ -11,4 +13,18 @@ package com.wnswdwy.niuke.number;
  */
 public class _45_ArrayToCombineMinNumber {
 
+
+    public String PrintMinNumber(int[] numbers) {
+        if (numbers == null || numbers.length == 0)
+            return "";
+        int n = numbers.length;
+        String[] nums = new String[n];
+        for (int i = 0; i < n; i++)
+            nums[i] = numbers[i] + "";
+        Arrays.sort(nums, (s1, s2) -> (s1 + s2).compareTo(s2 + s1));
+        String ret = "";
+        for (String str : nums)
+            ret += str;
+        return ret;
+    }
 }
